4d38bb505851550a2a8576bf3673208c02b7ef80
YasserSoltan <yassersoltan64@gmail.com>
2025-08-17 22:48:05 +0300

    signin user

--- lib/db/seed.ts
+++ lib/db/seed.ts
@@ -1,28 +1,33 @@
-import data from '@/lib/data'
-import { connectToDatabase } from '.'
-import Product from './models/product.model'
-import { cwd } from 'process'
-import { loadEnvConfig } from '@next/env'
+import data from "@/lib/data";
+import { connectToDatabase } from ".";
+import Product from "./models/product.model";
+import { cwd } from "process";
+import { loadEnvConfig } from "@next/env";
+import User from "./models/user.model";
 
-loadEnvConfig(cwd())
+loadEnvConfig(cwd());
 
 const main = async () => {
   try {
-    const { products } = data
-    await connectToDatabase(process.env.MONGODB_URI)
+    const { users, products } = data;
+    await connectToDatabase(process.env.MONGODB_URI);
 
-    await Product.deleteMany()
-    const createdProducts = await Product.insertMany(products)
+    await Product.deleteMany();
+    const createdProducts = await Product.insertMany(products);
+
+    await User.deleteMany();
+    const createdUser = await User.insertMany(users);
 
     console.log({
       createdProducts,
-      message: 'Seeded database successfully',
-    })
-    process.exit(0)
+      createdUser,
+      message: "Seeded database successfully",
+    });
+    process.exit(0);
   } catch (error) {
-    console.error(error)
-    throw new Error('Failed to seed database')
+    console.error(error);
+    throw new Error("Failed to seed database");
   }
-}
+};
 
-main()
+main();
