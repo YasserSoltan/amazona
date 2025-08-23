fa287583a4fc553f332fca62e566caca0928b0e7
YasserSoltan <yassersoltan64@gmail.com>
2025-08-23 14:13:48 +0300

    add theme color

--- components/shared/ClientProviders.tsx
+++ components/shared/ClientProviders.tsx
@@ -1,27 +1,30 @@
-'use client'
-import React from 'react'
-import useCartSidebar from '@/hooks/use-cart-sidebar'
-import CartSidebar from './CartSidebar'
-import { Toaster } from '../ui/sonner'
+"use client";
+import React from "react";
+import useCartSidebar from "@/hooks/use-cart-sidebar";
+import CartSidebar from "./CartSidebar";
+import { Toaster } from "../ui/sonner";
+import { ThemeProvider } from "./ThemeProvider";
 
 export default function ClientProviders({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  const isCartSidebarOpen = useCartSidebar()
+  const isCartSidebarOpen = useCartSidebar();
 
   return (
     <>
-      {isCartSidebarOpen ? (
-        <div className='flex min-h-screen'>
-          <div className='flex-1 overflow-hidden'>{children}</div>
-          <CartSidebar />
-        </div>
-      ) : (
-        <div>{children}</div>
-      )}
-      <Toaster />
+      <ThemeProvider attribute="class" defaultTheme="dark">
+        {isCartSidebarOpen ? (
+          <div className="flex min-h-screen">
+            <div className="flex-1 overflow-hidden">{children}</div>
+            <CartSidebar />
+          </div>
+        ) : (
+          <div>{children}</div>
+        )}
+        <Toaster />
+      </ThemeProvider>
     </>
-  )
+  );
 }
